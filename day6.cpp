7ae8513297c5d22f7bad4e668423a328e444bde0
Alice Harpole <aliceharpole@gmail.com>
2021-12-08 06:50:50 -0500

    Day 7 advent of code

--- day6.cpp
+++ day6.cpp
@@ -8,28 +8,28 @@
 using namespace std;
 
 long long evolveLanternfish(vector<int> initialAges, int nDays=80) {
-    // we're going to create a map that lists the numbers of laternfish of each age
-    deque<long long> laternfishAges(9, 0);
+    // we're going to create a map that lists the numbers of lanternfish of each age
+    deque<long long> lanternfishAges(9, 0);
 
     // now update the map with the initial fish age list
     for (const auto& age : initialAges) {
-        laternfishAges[age]++;
+        lanternfishAges[age]++;
     }
 
     // now we evolve for nDays
     for (int i = 0; i < nDays; ++i) {
         // pop off the fish with age 0
-        auto pregnantFish = laternfishAges.front();
-        laternfishAges.pop_front();
+        auto pregnantFish = lanternfishAges.front();
+        lanternfishAges.pop_front();
 
         // add these back with timers of 6 and add on new fish with timers of 8
-        laternfishAges[6] += pregnantFish;
-        laternfishAges.emplace_back(pregnantFish);
+        lanternfishAges[6] += pregnantFish;
+        lanternfishAges.emplace_back(pregnantFish);
     }
 
     // sum up queue to get total number of fish
     long long nFish = 0;
-    for (const auto& fish : laternfishAges) {
+    for (const auto& fish : lanternfishAges) {
         nFish += fish;
     }
 
@@ -42,9 +42,9 @@ int main() {
 
     auto nFish = evolveLanternfish(ages);
 
-    cout << "After 80 days, there are " << nFish << " laternfish." << endl;
+    cout << "After 80 days, there are " << nFish << " lanternfish." << endl;
 
     nFish = evolveLanternfish(ages, 256);
 
-    cout << "After 256 days, there are " << nFish << " laternfish." << endl;
+    cout << "After 256 days, there are " << nFish << " lanternfish." << endl;
 }
